99cb4fd4d0f4981616ab6ff83f640cef0c4a7ae2
Gosia-Parveen <gosiasandalparveen@gmail.com>
2026-08-30 22:29:50 +0530

    Add new URL paths for admin and account manager
    Implemented url paths for corresponding views

--- roles/urls.py
+++ roles/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.login,name="login")
+    path('', views.login, name="login"),
+    path('admin-dashboard/', views.admin_dash, name="admin_dash"),
+    path('account-manager/', views.acc_man, name="acc_man"),
 ]
