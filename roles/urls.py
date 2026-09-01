c5be3b1192126b9502c0c0fca7dc038aace85859
Gosia-Parveen <gosiasandalparveen@gmail.com>
2026-09-01 10:39:25 +0530

    Add subscription management URLs + logout
    Added new URL patterns for subscription management and logout for both admin and manager

--- roles/urls.py
+++ roles/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name="login"),
+
     path('admin-dashboard/', views.admin_dash, name="admin_dash"),
     path('account-manager/', views.acc_man, name="acc_man"),
+    path('subscript/', views.subs_tab, name="subs_tab"),
+    path("subscript/add/", views.add_subscription,name="add_subscription" ),
+    path("subscript/edit/<int:id>/", views.edit_subscription, name="edit_subscription"), 
+
+    path('logout/', views.logout, name="logout"),
 ]
