d32b71eae88a149b9ba2c181cf42ba9476a8227a
Gosia-Parveen <gosiasandalparveen@gmail.com>
2026-09-01 10:36:48 +0530

    Create Subscription model
    Added Subscription model with fields for customer details, billing, and ownership.

--- roles/models.py
+++ roles/models.py
@@ -1,3 +1,36 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Subscription(models.Model):
+
+    customer_name = models.CharField(max_length=100)
+    billing_email = models.EmailField()
+    plan = models.CharField(max_length=100)
+
+    billing_cycle = models.CharField(
+        max_length=20,
+        choices=[
+            ("Monthly", "Monthly"),
+            ("Yearly", "Yearly"),
+        ]
+    )
+
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    start_date = models.DateField()
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT
+    )
+
+    is_archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.customer_name
