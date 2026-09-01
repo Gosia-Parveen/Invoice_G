98c08531f8f821d73fbd01d37e459e065edbb4b3
Gosia-Parveen <gosiasandalparveen@gmail.com>
2026-09-01 21:25:03 +0530

    Remove unnecessary whitespace in urls.py

--- roles/urls.py
+++ roles/urls.py
@@ -3,17 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name="login"),
-
     path('admin-dashboard/', views.admin_dash, name="admin_dash"),
     path('account-manager/', views.acc_man, name="acc_man"),
     path('logout/', views.logout, name="logout"),
-    
-    
-    # ----------------------------------------------subscription---------------------------------------
+
+    # ----------------------------------------------Subscription---------------------------------------
     path('subscript/', views.subs_tab, name="subs_tab"),
     path("subscript/add/", views.add_subscription,name="add_subscription" ),
     path("subscript/edit/<int:id>/", views.edit_subscription, name="edit_subscription"), 
-    
+
     # ----------------------------------------------Invoice---------------------------------------
     path('invoices/',views.invoice_list,name='invoice_list'),
 
@@ -29,5 +27,5 @@ urlpatterns = [
 
     path('invoices/<int:invoice_id>/credit-note/',views.create_credit_note,name='create_credit_note'),
 
-
+    
 ]
